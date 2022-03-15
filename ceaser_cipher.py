25864d647d92c3a2d68fdbe64556c1f38a89ce90
katethegreat1 <katehanthegreat1@gmail.com>
2022-03-15 21:20:44 +1300

    add ceaserCipher

---
+++ ceaser_cipher.py
@@ -0,0 +1,35 @@
+#CeasarCipher
+
+def generate_key(num):
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    ceaser_key = {}
+    count = 0
+    for i in letters:
+        ceaser_key[i] = letters[(count+num) % len(letters)]
+        count += 1
+    return ceaser_key
+    
+ceaser_key = generate_key(5)
+print(ceaser_key)
+
+
+
+
+def generate_key(num):
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    ceaser_key = {}
+    for i in letters:
+        ceaser_key[i] = letters[(num)]
+        
+        return ceaser_key           # {'A': 'F'}
+        return i                    # A    - dict_key
+        return ceaser_key[i]        # F    - dict_value
+    
+    
+ceaser_key = generate_key(5)                   # key(100) -- IndexError: string index out of range
+print(ceaser_key)
+
+
+
+    
+    
